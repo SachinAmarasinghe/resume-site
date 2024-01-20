b28b25c0922c2e567de1cad6b54fe3a5c089692f
SachinAmarasinghe <sachinamarasinghe@outlook.com>
2024-01-20 15:31:42 -0500

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { ReferencesComponent } from './components/references/references.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    NavigationComponent,
+    AboutComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    ExperienceComponent,
+    EducationComponent,
+    ReferencesComponent,
+    ContactComponent,
+    FooterComponent,
+    PortfolioPageComponent,
+    HomePageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
