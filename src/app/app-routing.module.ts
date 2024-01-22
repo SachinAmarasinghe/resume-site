cba928d275ff3a8fe47c10565044d728394c94cf
SachinAmarasinghe <sachinamarasinghe@outlook.com>
2024-01-21 19:01:35 -0500

    Navigation component styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
 
-const routes: Routes = [{ path: '', component: HomePageComponent }];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'portfolio', component: PortfolioPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
