fe0b20fa58f3836581c5f99648d51065446da418
SachinAmarasinghe <sachinamarasinghe@outlook.com>
2024-01-24 07:56:28 -0500

    education changes

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import jsondata from '../../../assets/data.json';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrl: './experience.component.scss'
+  styleUrl: './experience.component.scss',
 })
 export class ExperienceComponent {
-
+  experiences: any[] = jsondata.experience;
 }
