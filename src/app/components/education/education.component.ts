525f17d5e0378fe717d432fdc4793bd774a972c2
SachinAmarasinghe <sachinamarasinghe@outlook.com>
2024-01-24 20:29:04 -0500

    education changes

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import jsondata from '../../../assets/data.json';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  styleUrl: './education.component.scss'
+  styleUrl: './education.component.scss',
 })
 export class EducationComponent {
-
+  educations: any[] = jsondata.education;
 }
